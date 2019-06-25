b7176f7e895b4c04627ff3a44835d12fb79d413d
igkriv <ig.kriv@gmail.com>
2019-06-25 07:37:14 +0300

    Fully worked, need to add clean of xlsx file

--- b.php
+++ b.php
@@ -4,25 +4,6 @@ require '/home/krig/vendor/autoload.php';
 
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
-function filew() {
-
-  $inputFileName = 'ЗАЯВКА ВОДОСТОК.xlsx';
-  
-  $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($inputFileName);
-  $worksheet = $spreadsheet->getSheet(0);
-  $worksheet->getCell('K26')->setValue("1");
-  $worksheet->getCell('K27')->setValue("2");
-  $worksheet->getCell('K28')->setValue("0");
-  $worksheet->getCell('K29')->setValue("0");
-  $worksheet->getCell('K30')->setValue("0");
-  $worksheet->getCell('K31')->setValue("0");
-  $worksheet->getCell('K32')->setValue("0");
-  $worksheet->getCell('K33')->setValue("0");
-  $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
-  $writer->save($inputFileName);
-  header("Location: http://example1.com/%D0%97%D0%90%D0%AF%D0%92%D0%9A%D0%90%20%D0%92%D0%9E%D0%94%D0%9E%D0%K1%D0%A2%D0%9E%D0%9A.xlsx");
-}
-
 if ($_POST) {
   
   $dlinazh3 = htmlspecialchars(print_r($_POST['dlinazh3'], true));
@@ -37,16 +18,46 @@ if ($_POST) {
   // $row it's row value to add to function filew()
   switch ($diam) {
     case "125":
-      $row = 26;
+      $rowZH3 = 26;
+      $rowZH4 = 27;
+      $rowMUF = 28;
+      $rowSLV110 = 29;
+      $rowSLV90 = 30;
+      $rowVNUG = 31;
+      $rowVNESHUG = 32;
+      $rowDERZH = 33;
+      $rowZAGLP = 34;
+      $rowZAGLL = 35;
       break;
     case "150":
-      $row = 38;
+      $rowZH4 = 38;
+      $rowMUF = 39;
+      $rowSLV110 = 40;
+      $rowVNUG = 41;
+      $rowVNESHUG = 42;
+      $rowDERZH = 43;
+      $rowZAGLP = 44;
+      $rowZAGLL = 45;
       break;
     case "100":
-      $row = 17;
+      $rowZH3 = 17;
+      $rowMUF = 18;
+      $rowSLV90 = 19;
+      $rowVNUG = 20;
+      $rowVNESHUG = 21;
+      $rowDERZH = 22;
+      $rowZAGLP = 23;
+      $rowZAGLL = 24;
       break;
     case "75":
-      $row = 8;
+      $rowZH3 = 8;
+      $rowMUF = 9;
+      $rowSLV90 = 10;
+      $rowVNUG = 11;
+      $rowVNESHUG = 12;
+      $rowDERZH = 13;
+      $rowZAGLP = 14;
+      $rowZAGLL = 15;
       break;
   }
   // $col column value to be equivalent to cvet
@@ -79,13 +90,26 @@ if ($_POST) {
   // $row1 it's row value to add to function filew()
   switch ($diam1) {
     case "90":
-      $row1 = 57;
+      $rowTR = 57;
+      $rowTR4 = 58;
+      $rowKN = 59;
+      $rowKOL = 60;
+      $rowKOL2 = 61;
+      $rowHOM = 62;
       break;
     case "110":
-      $row1 = 47;
+      $rowTR = 47;
+      $rowTR4 = 48;
+      $rowKN = 49;
+      $rowKOL = 50;
+      $rowKOL2 = 51;
+      $rowHOM = 52;
       break;
     case "63":
-      $row1 = 66;
+      $rowTR = 66;
+      $rowKN = 67;
+      $rowKOL = 68;
+      $rowHOM = 69;
       break;
   }
   // $col column value to be equivalent to cvet
@@ -115,16 +139,16 @@ if ($_POST) {
       $col1 = 'U';
       break;
   }
-  echo "<br><br><div align='left'>___служебный___<br>\$row value = ", $row;
-  echo "<br>\$col value (цвет желоба) = ", $col;
-  echo "<br>\$row1 value = ", $row1;
-  echo "<br>\$col1 value (цвет трубы) = ", $col1, "<br>___служебный___<br><br>";
-  echo "длина желоба 3 м - ", $dlinazh3, "<br>";
-  echo "длина желоба 4 м - ", $dlinazh4, "<br>";
-  echo "диамер желоба - ", $diam, "<br>";
-  echo "длина трубы 3 м - ", $dlinatr3, "<br>";
-  echo "длина трубы 4 м - ", $dlinatr4, "<br>";
-  echo "диаметр трубы - ", $diam1, "</div><br>";
+
+  echo "<form action='s.php' method='post'><input id='secinput' class='fi forma' type='submit' value='download excel file'></form>";
+  echo "<br><br><div align='left'>___служебный___<br>\$col (цвет желоба) = ", $col;
+  echo "<br>\$col1 (цвет трубы) = ", $col1, "<br>___служебный___<br><br>";
+  echo "\$diam (диамер желоба) - ", $diam, " мм <br>";
+  echo "\$dlinazh3 (длина желоба 3 м) - ", $dlinazh3, " м <br>";
+  echo "\$dlinazh4 (длина желоба 4 м) - ", $dlinazh4, " м <br><br>";
+  echo "\$diam1 (диаметр трубы) - ", $diam1, " мм <br>";
+  echo "\$dlinatr3 (длина трубы 3 м) - ", $dlinatr3, " м <br>";
+  echo "\$dlinatr4 (длина трубы 4 м) - ", $dlinatr4, " м </div><br>";
 
   $mufta = (int)($dlinazh3/3 + $dlinazh4/4);
   $slivvor = (int)(($dlinazh3 + $dlinazh4)/3); // Сливная воронка ( 3 ) 75/Ø63
@@ -137,23 +161,60 @@ if ($_POST) {
   $koleno = (int)(($dlinazh3 + $dlinazh4)/3); // Колено  67,5 st. ( 12 )
   $koleno2 = (int)($koleno/5); // Колено  67,5 st. ( 13 ) двухраструбное 
   $khomut = (int)($dlinatr3/3 + $dlinatr4/4); // Хомут водосточной трубы ( 14 )
-  // $luk = 0; // Люк для чистки  ( 15 ) 
-  // $troinik110 = 0; // Тройник 110/110/110, 67,6 st.( 16 )
-  // $troinik63 = 0; // Тройник 110/110/63, 67,6 st.( 16 )
 
-  echo "<div align='left'>муфта желоба - ", $mufta, "<br>";
-  echo "сливная воронка - ", $slivvor, "<br>";
-  echo "внурений угол - ", $vnutrugol, "<br>";
-  echo "внешний угол - ", $vneshrugol, "<br>";
-  echo "держатель желоба - ", $derzhzel, "<br>";
-  echo "заглушка правая - ", $zaglprav, "<br>";
-  echo "заглушка левая - ", $zagllev, "<br>";
-  echo "кольцевой ниппель - ", $kolcnip, "<br>";
-  echo "колено - ", $koleno, "<br>";
-  echo "колено двухраструбное - ", $koleno2, "<br>";
-  echo "хомут трубы - ", $khomut, "<br>";
+  echo "<div align='left'>\$mufta (муфта желоба) - ", $mufta, " шт.<br>";
+  echo "\$slivvor (сливная воронка) - ", $slivvor, " шт.<br>";
+  echo "\$vnutrugol (внурений угол) - ", $vnutrugol, " шт.<br>";
+  echo "\$vneshrugol (внешний угол) - ", $vneshrugol, " шт.<br>";
+  echo "\$derzhzel (держатель желоба) - ", $derzhzel, " шт.<br>";
+  echo "\$zaglprav (заглушка правая) - ", $zaglprav, " шт.<br>";
+  echo "\$zagllev (заглушка левая) - ", $zagllev, " шт.<br><br>";
+  echo "\$kolcnip (кольцевой ниппель) - ", $kolcnip, " шт.<br>";
+  echo "\$koleno (колено) - ", $koleno, " шт.<br>";
+  echo "\$koleno2 (колено двухраструбное) - ", $koleno2, " шт.<br>";
+  echo "\$khomut (хомут трубы) - ", $khomut, " шт.<br>";
   // echo "luk - ", $luk, "<br>";
   // echo "troinik110 - ", $troinik110, "<br>";
   // echo "troinik63 - ", $troinik63, "<br>";
   echo "</div>";
+
+  # блок вставки данных в файл
+  $inputFileName = 'ЗАЯВКА ВОДОСТОК.xlsx';
+  
+  $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($inputFileName);
+  $worksheet = $spreadsheet->getSheet(0);
+  
+  # вставляет данные в ячейки ЗАЯВКА ВОДОСТОК.xlsx
+  if ($rowZH3) {
+    $worksheet->getCell($col.$rowZH3)->setValue($dlinazh3); # кол-во 3м желоба
+  }
+  if ($rowZH4) {
+    $worksheet->getCell($col.$rowZH4)->setValue($dlinazh4); # кол-во 4м желоба
+  }
+  $worksheet->getCell($col.$rowMUF)->setValue($mufta); # кол-во муфты
+  if ($rowSLV90) {
+    $worksheet->getCell($col.$rowSLV90)->setValue($slivvor); # кол-во сливной воронки малой
+  }
+  if ($rowSLV110) {
+    $worksheet->getCell($col.$rowSLV110)->setValue($slivvor); # кол-во сливной воронки большой
+  }
+  $worksheet->getCell($col.$rowVNUG)->setValue($vnutrugol); # кол-во внутрений угол
+  $worksheet->getCell($col.$rowVNESHUG)->setValue($vneshrugol); # кол-во внешний угол
+  $worksheet->getCell($col.$rowDERZH)->setValue($derzhzel); # кол-во держателей
+  $worksheet->getCell($col.$rowZAGLP)->setValue($zaglprav); # кол-во заглушек правых
+  $worksheet->getCell($col.$rowZAGLL)->setValue($zagllev); # кол-во заглушек левых
+  
+  $worksheet->getCell($col1.$rowTR)->setValue($dlinatr3); # кол-во 3 м трубы
+  if ($rowTR4) {
+    $worksheet->getCell($col1.$rowTR4)->setValue($dlinatr4); # кол-во 4 м трубы
+  }
+  $worksheet->getCell($col1.$rowKN)->setValue($kolcnip); # кол-во кольцевого ниппеля
+  $worksheet->getCell($col1.$rowKOL)->setValue($koleno); # кол-во коленей обычных
+  if ($rowKOL2) {
+    $worksheet->getCell($col1.$rowKOL2)->setValue($koleno2); # кол-во коленей двухраструбных
+  }
+  $worksheet->getCell($col1.$rowHOM)->setValue($khomut); # кол-во коленей обычных
+  
+  $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
+  $writer->save($inputFileName);
 }
--- i.php
+++ i.php
@@ -1,1 +1,80 @@
-<?php phpinfo(); ?>
+<?php
+
+require '/home/krig/vendor/autoload.php';
+
+use PhpOffice\PhpSpreadsheet\IOFactory;
+
+$rowDERZH = 1;
+$rowHOM = 1;
+$rowKN = 1;
+$rowKOL = 1;
+$rowKOL2 = 1;
+$rowMUF = 1;
+$rowSLV110 = 1;
+$rowSLV90 = 1;
+$rowTR = 1;
+$rowTR4 = 1;
+$rowVNESHUG = 1;
+$rowVNUG = 1;
+$rowZAGLL = 1;
+$rowZAGLP = 1;
+$rowZH3 = 1;
+$rowZH3 = 1;
+$rowZH4 = 1;
+$col = 'G';
+$col1 = 'G';
+$khomut = 1;
+$kolcnip = 1;
+$koleno = 1;
+$koleno2 = 1;
+$derzhzel = 1;
+$dlinatr3 = 1;
+$dlinatr4 = 1;
+$dlinazh3 = 1;
+$dlinazh4 = 1;
+$mufta = 1;
+$slivvor = 1;
+$vneshrugol = 1;
+$vnutrugol = 1;
+$zagllev = 1;
+$zaglprav = 1;
+
+# блок вставки данных в файл
+$inputFileName = 'ЗАЯВКА ВОДОСТОК.xlsx';
+
+$spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($inputFileName);
+$worksheet = $spreadsheet->getSheet(0);
+
+# вставляет данные в ячейки ЗАЯВКА ВОДОСТОК.xlsx
+if ($rowZH3) {
+  $worksheet->getCell($col.$rowZH3)->setValue($dlinazh3); # кол-во 3м желоба
+}
+if ($rowZH4) {
+  $worksheet->getCell($col.$rowZH3)->setValue($dlinazh4); # кол-во 4м желоба
+}
+$worksheet->getCell($col.$rowMUF)->setValue($mufta); # кол-во муфты
+if ($rowSLV90) {
+  $worksheet->getCell($col.$rowSLV90)->setValue($slivvor); # кол-во сливной воронки малой
+}
+if ($rowSLV110) {
+  $worksheet->getCell($col.$rowSLV110)->setValue($slivvor); # кол-во сливной воронки большой
+}
+$worksheet->getCell($col.$rowVNUG)->setValue($vnutrugol); # кол-во внутрений угол
+$worksheet->getCell($col.$rowVNESHUG)->setValue($vneshrugol); # кол-во внешний угол
+$worksheet->getCell($col.$rowDERZH)->setValue($derzhzel); # кол-во держателей
+$worksheet->getCell($col.$rowZAGLP)->setValue($zaglprav); # кол-во заглушек правых
+$worksheet->getCell($col.$rowZAGLL)->setValue($zagllev); # кол-во заглушек левых
+
+$worksheet->getCell($col1.$rowTR)->setValue($dlinatr3); # кол-во 3 м трубы
+if ($rowTR4) {
+  $worksheet->getCell($col1.$rowTR4)->setValue($dlinatr4); # кол-во 4 м трубы
+}
+$worksheet->getCell($col1.$rowKN)->setValue($kolcnip); # кол-во кольцевого ниппеля
+$worksheet->getCell($col1.$rowKOL)->setValue($koleno); # кол-во коленей обычных
+if ($rowKOL2) {
+  $worksheet->getCell($col1.$rowKOL2)->setValue($koleno2); # кол-во коленей двухраструбных
+}
+$worksheet->getCell($col1.$rowHOM)->setValue($khomut); # кол-во коленей обычных
+
+$writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
+$writer->save($inputFileName);
